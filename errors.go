4b575105fa30362836064e03c91ab00e4ae81984
evolsnow <gsc1215225@gmail.com>
2016-03-30 14:31:26 +0800

    err msg

--- errors.go
+++ errors.go
@@ -9,21 +9,21 @@ import (
 // This file shamelessly adapted from martini-contrib/binding
 
 type (
-	// Errors may be generated during deserialization, binding,
-	// or validation. It implements the built-in error interface.
+// Errors may be generated during deserialization, binding,
+// or validation. It implements the built-in error interface.
 	Errors []Error
 
-	// Error is a powerful implementation of the built-in error
-	// interface that allows for error classification, custom error
-	// messages associated with specific fields, or with no
-	// associations at all.
+// Error is a powerful implementation of the built-in error
+// interface that allows for error classification, custom error
+// messages associated with specific fields, or with no
+// associations at all.
 	Error struct {
 		// An error supports zero or more field names, because an
 		// error can morph three ways: (1) it can indicate something
 		// wrong with the request as a whole, (2) it can point to a
 		// specific problem with a particular input field, or (3) it
 		// can span multiple related input fields.
-		FieldNames []string `json:"fieldNames,omitempty"`
+		FieldNames     []string `json:"fieldNames,omitempty"`
 
 		// The classification is like an error code, convenient to
 		// use when processing or categorizing an error programmatically.
@@ -35,7 +35,7 @@ type (
 		// example, a payload of 100 objects in a JSON array might have
 		// an error in the 41st object. The message should help the
 		// end user find and fix the error with their request.
-		Message string `json:"message,omitempty"`
+		Message        string `json:"message,omitempty"`
 	}
 )
 
@@ -83,8 +83,8 @@ func (e Errors) Handle(response http.ResponseWriter) bool {
 		}
 		reply := map[string]interface{}{
 			"code":   status,
-			"error":  e[0].Classification,
-			"detail": e,
+			"msg":  e[0].Classification,
+			//"detail": e,
 		}
 		errOutput, _ := json.Marshal(reply)
 		response.WriteHeader(status)
@@ -122,8 +122,8 @@ func (e Error) Error() string {
 }
 
 const (
-	RequiredError        = "RequiredError"
-	ContentTypeError     = "ContentTypeError"
+	RequiredError = "RequiredError"
+	ContentTypeError = "ContentTypeError"
 	DeserializationError = "DeserializationError"
-	TypeError            = "TypeError"
+	TypeError = "TypeError"
 )
